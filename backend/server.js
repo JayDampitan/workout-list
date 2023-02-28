bb29f9ebcd36011bc9b15cdc77f20faa4df0b62e
JayDampitan <jaykennethpd@gmail.com>
2023-02-28 10:32:26 -0800

    create a function to connect to mongodb. install colors package for clearer console url display.

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const workoutRoutes = require("./routes/workoutRoutes");
+
+connectDB();
 
 const app = express();
 
-
-// express middles for 
+// express middles for
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/workouts", workoutRoutes);
 
-
 //middleware for status code
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
