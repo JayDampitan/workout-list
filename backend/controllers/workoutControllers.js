8ad2dd7636ca487a2ebb48cab5416b824a5ea99b
JayDampitan <jaykennethpd@gmail.com>
2023-02-28 10:04:50 -0800

    apply express middleware and create another middleware for the statuscode check.

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -1,13 +1,35 @@
-const getAllWorkouts = (req, res) =>
-  res.status(200).json({ message: "get all workouts" });
+const asyncHandler = require("express-async-handler");
 
-const createWorkout = (req, res) =>
+//@desc Get all workouts
+//@route GET /api/workouts/
+//@access Public
+const getAllWorkouts = asyncHandler(async (req, res) => {
+  const { title, weight, reps, date } = req.body;
+
+  if(!title || !weight || !reps || !date) {
+    res.status(400)
+    throw new Error("Please add all fields")
+  }
+
+  res.status(200).json({ message: "get all workouts" });
+});
+
+//@desc Create a workout
+//@route POST /api/workouts/
+//@access Public
+const createWorkout = async (req, res) =>
   res.status(200).json({ message: "create a workout" });
 
-const updateWorkout = (req, res) =>
-  res.status(200).json({ message:` update a workout ${req.params.id}`});
+//@desc Get all workouts
+//@route GET /api/workouts/
+//@access Public
+const updateWorkout = async (req, res) =>
+  res.status(200).json({ message: ` update a workout ${req.params.id}` });
 
-const deleteWorkout = (req, res) =>
+//@desc Get all workouts
+//@route GET /api/workouts/
+//@access Public
+const deleteWorkout = async (req, res) =>
   res.status(200).json({ message: `delete a workout ${req.params.id}` });
 
 module.exports = {
