ddf2d54b84e12b2974f6a339cbca3a6800d6c211
JayDampitan <jaykennethpd@gmail.com>
2023-03-01 12:09:15 -0800

    create generateToken function and apply to registerUser and loginUser controllers.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -58,6 +59,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -73,5 +75,13 @@ const getUserData = asyncHandler(async (req, res) => {
 
   res.status(200).json(users)
 });
+
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: "30d"
+  })
+}
 
 module.exports = { registerUser, loginUser, getUserData };
