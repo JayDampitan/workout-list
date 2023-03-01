292513874ace888b60700b1a8ae87967d2ccc510
JayDampitan <jaykennethpd@gmail.com>
2023-03-01 12:02:22 -0800

    apply functionality in the loginUser controller and update gitignore.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,14 +49,29 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "login user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 //@desc Authenticate User
 //@route POST /api/users/login
 //@access Public
 const getUserData = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User Data" });
+  const users = await User.find()
+
+  res.status(200).json(users)
 });
 
 module.exports = { registerUser, loginUser, getUserData };
