5ad3697ad5f5445f6f0966e36ff6e95ef3e1d298
JayDampitan <jaykennethpd@gmail.com>
2023-03-01 11:26:13 -0800

    apply bcrypt password hashing to the registerUser controller. add user model to the workout model.add another route and controller to display user data only.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,62 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
+//@desc Create a user
+//@route POST /api/users/
+//@access Public
 const registerUser = asyncHandler(async (req, res) => {
-const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-if(!name || !email || !password ) {
-    res.status(400)
-    throw new Error("Please add all fiels")
-}
-    
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
 
-  res.status(200).json({ message: "register user" });
+  // Check if the user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
-
+//@desc Authenticate User
+//@route POST /api/users/login
+//@access Public
 const loginUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "login user" });
 });
 
-module.exports = { registerUser, loginUser };
+//@desc Authenticate User
+//@route POST /api/users/login
+//@access Public
+const getUserData = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Data" });
+});
+
+module.exports = { registerUser, loginUser, getUserData };
